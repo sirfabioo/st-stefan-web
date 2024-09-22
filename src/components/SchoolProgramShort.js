88184b1b00f99a8a27a22612a9e8063a487966aa
sirfabio <fabio.unterholzer@outlook.com>
2024-09-22 20:10:26 +0200

    wip

--- src/components/SchoolProgramShort.js
+++ src/components/SchoolProgramShort.js
@@ -4,20 +4,19 @@ import '../styles/SchoolProgramShort.css';  // Import the custom styles
 
 const SchoolProgramShort = () => {
   return (
-    <div className="program-icons-container">
-      <div className="icon-circle">
-        <i className="fas fa-book"></i>  {/* First icon */}
+    <div>
+      
+      <div className="simple-bar">
+        <h2 className="bar-heading">Entdecken Sie unser Schulprogramm</h2>
       </div>
-      <div className="icon-circle">
-        <i className="fas fa-chalkboard-teacher"></i>  {/* Second icon */}
+      
+      <div className="simple-bar-variant">
+        <h2 className="bar-heading-variant">Lorem Ipsum</h2>
       </div>
-      <div className="icon-circle">
-        <i className="fas fa-basketball-ball"></i>  {/* Third icon */}
-      </div>
-      <div className="icon-circle">
-        <Link to="/schoolinfo" className="cta-btn">Mehr erfahren</Link>  {/* "Mehr erfahren" button */}
-      </div>
+
     </div>
+    
+
   );
 };
 
